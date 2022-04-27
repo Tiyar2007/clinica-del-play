75bdfc387808a5dd91f12909de0e3716950080a4
tiyar2007 <a>
2022-04-27 17:19:11 -0500

    Se añaden js

--- javabon.js
+++ javabon.js
@@ -1,1 +1,128 @@
-let pabon = Array ("tilin")
+let producto1 = {
+    nombre: "PS1" ,
+    estado: "Segunda" ,
+    Precio: 150000 ,
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-ac341.appspot.com/o/ps1.png?alt=media&token=5cdd6d8c-833f-4254-8992-45239dd57c3c"
+}
+
+let producto2 = {
+    nombre: "PS2" ,
+    estado: "Segunda" ,
+    Precio: 300000 ,
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-ac341.appspot.com/o/ps2.png?alt=media&token=2e537f88-7b60-4e54-8fdf-71b7bdd7544f"
+}
+
+let producto3 = {
+    nombre: "PS3" ,
+    estado: "Nueva" ,
+    Precio: 600000 ,
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-ac341.appspot.com/o/ps3.png?alt=media&token=859a9f1e-9651-4649-8d9c-18293288405c"
+}
+
+let producto4 = {
+    nombre: "PS4" ,
+    estado: "Nueva" ,
+    Precio: 900000 ,
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-ac341.appspot.com/o/ps4.png?alt=media&token=6a750080-7813-44b5-90e3-aaf063f0b65f"
+}
+
+let producto5 = {
+    nombre: "PS5" ,
+    estado: "Nueva" ,
+    Precio: 3000000 ,
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-ac341.appspot.com/o/ps5.jpg?alt=media&token=52f77a47-e0f4-4f78-959d-6527f46d3d07"
+}
+
+let producto6 = {
+    nombre: "Control PS1" ,
+    estado: "Segunda" ,
+    Precio: 25000 ,
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-ac341.appspot.com/o/controlps1.png?alt=media&token=91a897d4-6e75-45c0-9c10-caa85633e795"
+}
+
+let producto7 = {
+    nombre: "Control PS2" ,
+    estado: "Segunda" ,
+    Precio: 30000 ,
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-ac341.appspot.com/o/controllps2.png?alt=media&token=c3abe3ce-dd54-4263-81b5-102baf20a38f"
+}
+
+let producto8 = {
+    nombre: "Control PS3" ,
+    estado: "Nuevo" ,
+    Precio: 70000 ,
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-ac341.appspot.com/o/controlps3.png?alt=media&token=e1ca2e3b-4ae8-4fa7-9a00-b5ad4c1a4e15"
+}
+
+let producto9 = {
+    nombre: "Control PS4" ,
+    estado: "Nuevo" ,
+    Precio: 150000 ,
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-ac341.appspot.com/o/controllps4.png?alt=media&token=75241a2c-49ce-4241-98c2-9e2c36a63e3b"
+}
+
+let producto10 = {
+    nombre: "Control PS5" ,
+    estado: "Nuevo" ,
+    Precio: 700000 ,
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-ac341.appspot.com/o/controlps5.png?alt=media&token=7bf3451b-1893-4efd-be72-5068bc407fc3"
+}
+
+let producto11 = {
+    nombre: "Nintendo switch" ,
+    estado: "Nueva" ,
+    Precio: 1500000 ,
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-ac341.appspot.com/o/swith.png?alt=media&token=43135ad9-d4b0-4fc7-ad71-ce16e63d93f1"
+}
+
+let producto12 = {
+    nombre: "Control Nintendo switch" ,
+    estado: "Nuevo" ,
+    Precio: 130000 ,
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-ac341.appspot.com/o/controlswitch.png?alt=media&token=80549bd2-5272-444f-a747-7ecff1b25450"
+}
+
+let producto13 = {
+    nombre: "Uncharted play 4" ,
+    estado: "Nuevo" ,
+    Precio: 170000 ,
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-ac341.appspot.com/o/unchartred.png?alt=media&token=f0d53e78-1947-4ca1-89ab-6142b68d21ea"
+}
+
+let producto14 = {
+    nombre: "cable HDMI" ,
+    estado: "Nuevo" ,
+    Precio: 40000 ,
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-ac341.appspot.com/o/Cable.png?alt=media&token=35b95f93-f05b-4466-8e0d-19841e05b299"
+}
+
+let producto15 = {
+    nombre: "Cable de potencia" ,
+    estado: "Nuevo" ,
+    Precio: 27000 ,
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-ac341.appspot.com/o/cableps4.png?alt=media&token=946a593f-20a8-485f-8ae5-d82450339f5f"
+}
+
+let productos = Array (
+    producto1 ,
+    producto2 , 
+    producto3 , 
+    producto4 , 
+    producto5 ,
+    producto6 ,
+    producto7 ,
+    producto8 ,
+    producto9 ,
+    producto10 ,
+    producto11 ,
+    producto12 ,
+    producto13 ,
+    producto14 ,
+    producto15 
+    )
+
+
+
+    productos.forEach(function(producto){
+      console.log(producto.nombre)  
+    })
